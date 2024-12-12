bf01792242d343af15f019a4d6d0f0be25e5127b
Quocvan <lyquocvan1999@gmail.com>
2024-12-12 09:36:49 +0700

    config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Api airbnb')
+    .setDescription('Danh sách Api Airbnb')
+    .setVersion('1.0')
+    .build();
+
+  const swagger = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('swagger', app, swagger);
+
+  await app.listen(process.env.PORT ?? 8080);
 }
+
 bootstrap();
